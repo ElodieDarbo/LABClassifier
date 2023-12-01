7ca1ae231140833d29d943137e0f7373d354fde4
Elodie Darbo <elodie.darbo@u-bordeaux.fr>
2023-12-01 17:25:34 +0100

    Add link to pam.robust data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,14 @@
 #' }
 #' @source <https://tcga-xena-hub.s3.us-east-1.amazonaws.com/download/TCGA.BRCA.sampleMap%2FHiSeqV2.gz>
 "TCGA.rsem"
+
+#' Object from genefu package to compute PAM50 classification
+#'
+#'
+#' @format ## `pam50.robust`
+#' #' \describe{
+#' See genefu package (Deena M.A. Gendoo et al. (2021). genefu: Computation of Gene
+#' Expression-Based Signatures in Breast Cancer. R package version 2.26.0.)
+#' }
+#' @source <http://www.pmgenomics.ca/bhklab/software/genefu>
+"pam50.robust"
