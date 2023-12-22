5479efc5b577b0865d9f689ea057a0d5710d9bc9
Elodie Darbo <elodie.darbo@u-bordeaux.fr>
2023-12-22 12:38:48 +0100

    update confidence and classif method

--- R/functions.R
+++ R/functions.R
@@ -35,7 +35,8 @@ classify_splits <- function(d2,L,opt=NULL) {
     n <- ncol(L$centroids)
     d2 <- t(d2)
     tdist <- as.matrix(cor(t(d2),method="spearman"))
-    tdist <- as.data.frame(matrix(1-c(tdist[1,2],tdist[1,3]),ncol=2))
+    #tdist <- as.data.frame(matrix(1-c(tdist[1,2],tdist[1,3]),ncol=2))
+    tdist <- as.data.frame(matrix(1-as.vector(tdist[1,2:(length(classes)+1)]),ncol=length(classes)))
   } else {
     N <- ncol(d2)
     #d2 <- t(scale(t(d2),scale=F))
@@ -156,7 +157,9 @@ expression.dotplot <- function(data, predictions,g1,g2,confidence=F,PAM50=F){
     )
   if (confidence){
     data$confidence <- predictions$confidence[match(data$patientID,row.names(predictions))]
-    g <- g + geom_point(data=data,aes(color=confidence))
+    g <- g + geom_point(data=data,aes(color=confidence)) +
+      scale_color_gradientn(colours = c("yellow","orange","red","black"),limits=c(0,0.5),values = c(0,0.02,0.1,0.2,0.5,1))
+
   }
   else {
     g <- g + geom_point(aes(color=prediction))
@@ -196,6 +199,7 @@ expression.dotplot <- function(data, predictions,g1,g2,confidence=F,PAM50=F){
 #' Expression-Based Signatures in Breast Cancer. R package version 2.26.0.
 #' http://www.pmgenomics.ca/bhklab/software/genefu
 #' @import ggplot2
+#' @importFrom utils write.table
 #' @return \code{LABclassifier} Returns a data.frame with columns
 #' \item{sensory2secretory}{Distance to sensory centroid}
 #' \item{Luminal2Apocrine}{Distance to Luminal centroid}
@@ -215,6 +219,7 @@ expression.dotplot <- function(data, predictions,g1,g2,confidence=F,PAM50=F){
 
 
 LABclassifier <- function(data,LABClassif=NULL,raw.counts=F,log2T=F,id.type="SYMBOL",PAM50=F,plot=F,prefix=NULL){
+
   v <- F
   if (is.vector(data)){
     if (is.null(names(data))) stop("ERROR: You submitted a vector that is not named.")
@@ -267,9 +272,9 @@ LABclassifier <- function(data,LABClassif=NULL,raw.counts=F,log2T=F,id.type="SYM
   }
   if (raw.counts){
     message("Transforming RNA-seq raw counts into log2(TPM+1)")
-    m <- match(gene.length$SYMBOL,row.names(data))
-    ldata <- data[na.omit(m),,drop=F]
-    gene.length <- gene.length[!is.na(m),,drop=F]
+    m <- match(row.names(data),gene.length$SYMBOL)
+    ldata <- data[!is.na(m),,drop=F]
+    gene.length <- gene.length[na.omit(m),,drop=F]
     ldata <- (ldata / (gene.length$effective_length+1))*1000
     TPM <- apply(ldata,2,function(x){
       x <- (x/sum(x))*10^6
@@ -309,24 +314,106 @@ LABclassifier <- function(data,LABClassif=NULL,raw.counts=F,log2T=F,id.type="SYM
 
   pred.la <- classify_splits(data,centroid.la,"la")
 
-  message("Computing final predictions ...")
-  final.pred <- ifelse(pred.ss$pred.ss=="secretory","Basal",pred.la$pred.la)
-  final.pred.strict <- final.pred
-  final.pred.strict[pred.ss$pred.ss.strict=="unclassified" | (pred.la$pred.la.strict=="unclassified" & pred.ss$pred.ss.strict=="sensory")] <- "unclassified"
 
-  pred.final <- data.frame(sensory2secretory=pred.ss$sensory,Luminal2Apocrine=pred.la$Luminal,prediction=final.pred,prediction.strict=final.pred.strict,row.names=colnames(data))
-  pred.final$prediction.strict <- factor(as.vector(pred.final$prediction.strict),levels=c("Luminal","Basal","MA","unclassified"))
+  # s2s <- cut(pred.final$sensory2secretory,breaks = c(0,0.75,0.9,1.1,1.25,2))
+  # levels(s2s) <- c("high","medium","low","medium","high")
+  # l2a <- cut(pred.final$Luminal2Apocrine,breaks = c(0,0.75,0.9,1.1,1.25,2))
+  # levels(l2a) <- c("high","medium","low","medium","high")
+  # lab <- s2s
+  # lab[pred.ss$pred.ss=="sensory"] <- l2a[pred.ss$pred.ss=="sensory"]
+  # lab[lab!="low" & s2s=="low"] <- "low"
+  # lab[lab=="high" & s2s=="medium"] <- "medium"
+  # pred.final$confidence <- lab
+
+  #pred.final$combined.score <- pred.final$Luminal2Apocrine + pred.final$sensory2secretory
+
+  compute.pvalue <- function(x,y,class){
+    corner <- c(0,0)
+    switch(class,
+           luminal={
+             wa <- 0.8
+             wb <- 0.7
+           },
+           basal={
+             x <- 2 - x
+             y <- 2 - y
+             wa <- 1.2
+             wb <- 0
+           },
+           apocrine={
+             y <- 2 - y
+             wa <- 0.8
+             wb <- 0.7
+           }
+           )
+
+    distances <- sqrt(wa*(x-corner[1])^2+wb*(y-corner[2])^2)
+
+    compute_slope <- function(x1, y1, x2, y2) {
+      slope <- (y2 - y1) / (x2 - x1)
+      return(slope)
+    }
+
+    # Function to compute the angle at the crossing of two lines
+    compute_angle <- function(slope1, slope2) {
+      # Calculate the angle in radians
+      angle_rad <- atan(abs((slope2 - slope1) / (1 + slope1 * slope2)))
+
+      # Convert the angle to degrees
+      angle_deg <- angle_rad * 180 / pi
+
+      return(angle_deg)
+    }
+
+    distances <- apply(data.frame(x,y,distances),1,function(p){
+      x <- p[1]
+      y <- p[2]
+      distances <- p[3]
+      corner <- c(0,0)
+      slope1 <- compute_slope(0,0,1,0)
+      slope2 <- compute_slope(0,0,x,y)
+      angle <- compute_angle(slope1,slope2)
+      b <- 1
+      if (angle<45){
+        max.dist <- sqrt((b-corner[1])^2+(b*slope2-corner[2])^2)
+      } else {
+        max.dist <- sqrt((b/slope2-corner[1])^2+(b-corner[2])^2)
+      }
+      distances <- distances / max.dist
+
+      return(distances)
+    })
+    #print(plot(density(distances)))
+    round(pnorm(distances,mean = 0,sd = 0.3,lower.tail = F),7)
+
+    #pgamma(distances,shape = 1,scale = 0.25,lower.tail = F)
+  }
+
+  pred.final <- data.frame(sensory2secretory=pred.ss$sensory,Luminal2Apocrine=pred.la$Luminal,row.names=colnames(data))
 
   message("Computing confidence ...")
-  s2s <- cut(pred.final$sensory2secretory,breaks = c(0,0.75,0.9,1.1,1.25,2))
-  levels(s2s) <- c("high","medium","low","medium","high")
-  l2a <- cut(pred.final$Luminal2Apocrine,breaks = c(0,0.75,0.9,1.1,1.25,2))
-  levels(l2a) <- c("high","medium","low","medium","high")
-  lab <- s2s
-  lab[pred.ss$pred.ss=="sensory"] <- l2a[pred.ss$pred.ss=="sensory"]
-  lab[lab!="low" & s2s=="low"] <- "low"
-  lab[lab=="high" & s2s=="medium"] <- "medium"
-  pred.final$confidence <- lab
+  #sprintf("%.10f", data$Value)
+  pred.final$test.bas <- compute.pvalue(pred.final$sensory2secretory,pred.final$Luminal2Apocrine,"basal")
+  pred.final$test.lum <- compute.pvalue(pred.final$sensory2secretory,pred.final$Luminal2Apocrine,"luminal")
+  pred.final$test.apo <- compute.pvalue(pred.final$sensory2secretory,pred.final$Luminal2Apocrine,"apocrine")
+
+  pred.final$confidence <- apply(pred.final[,c("test.lum","test.apo","test.bas")],1,max)
+
+  message("Computing final predictions ...")
+  #final.pred <- ifelse(pred.ss$pred.ss=="secretory","Basal",pred.la$pred.la)
+  #final.pred.strict <- final.pred
+  #final.pred.strict[pred.ss$pred.ss.strict=="unclassified" | (pred.la$pred.la.strict=="unclassified" & pred.ss$pred.ss.strict=="sensory")] <- "unclassified"
+
+  pred.final$prediction <- apply(pred.final[,c("test.bas","test.lum","test.apo")],1,function(x){
+    c("Basal","Luminal","MA")[which.max(x)]
+  })
+
+  pred.final$prediction.strict <- ifelse(pred.final$test.apo>=0.01,"MA","unclassified")
+  pred.final$prediction.strict[pred.final$test.lum>=0.01] <- "Luminal"
+  pred.final$prediction.strict[pred.final$test.bas>=0.01] <- "Basal"
+
+  #pred.final <- data.frame(sensory2secretory=pred.ss$sensory,Luminal2Apocrine=pred.la$Luminal,prediction=final.pred,prediction.strict=final.pred.strict,row.names=colnames(data))
+  pred.final$prediction.strict <- factor(as.vector(pred.final$prediction.strict),levels=c("Luminal","Basal","MA","unclassified"))
 
   if (PAM50){
     message("Computing PAM50 classification")
@@ -344,7 +431,8 @@ LABclassifier <- function(data,LABClassif=NULL,raw.counts=F,log2T=F,id.type="SYM
       geom_segment(data=thrs.lines,aes(xend=xend,yend=yend),linetype=c("dashed","solid","dashed"),size=0.5,color="darkgrey") +
       labs(x="Distance to sensory centroid",y="Distance to Luminal centroid") + theme(aspect.ratio = 1,legend.position="top")
       g1 <- g + geom_point(aes(color=prediction.strict)) + scale_color_manual(values=c(Basal="red",Luminal="darkblue",MA="pink",unclassified="grey")) + geom_point(data=pred.final[pred.final$prediction.strict=="MA",],shape=1,color="purple")
-      g9 <- g + geom_point(data=pred.final,aes(color=confidence)) + scale_color_manual(values=c(high="red",medium="orange",low="grey"))
+      #g9 <- g + geom_point(data=pred.final,aes(color=confidence)) + scale_color_manual(values=c(high="red",medium="orange",low="grey"))
+      g9 <- g + geom_point(aes(color=confidence)) + scale_color_gradientn(colours = c("yellow","orange","red","black"),limits=c(0,0.5),values = c(0,0.02,0.1,0.2,0.5,1))
       if (PAM50){
         gpam <- g + geom_point(data=pred.final,aes(color=PAM50)) + scale_color_manual(values=c(Basal="red",LumA="darkblue",Her2="pink",LumB="lightblue")) + geom_point(data=pred.final[pred.final$PAM50=="Her2",],shape=1,color="deeppink")
       }
@@ -371,6 +459,12 @@ LABclassifier <- function(data,LABClassif=NULL,raw.counts=F,log2T=F,id.type="SYM
   if (!is.null(prefix)){
     message("If not existing, creating a folder 'Output' in the working directory: ",getwd())
     suppressWarnings(dir.create("./Output",recursive = T))
+    pred.final$test.bas <- sprintf("%.7f", pred.final$test.bas)
+    pred.final$test.lum <- sprintf("%.7f", pred.final$test.lum)
+    pred.final$test.apo <- sprintf("%.7f", pred.final$test.apo)
+
+    pred.final$confidence <- sprintf("%.7f", pred.final$confidence)
+
     write.table(pred.final,paste0("./Output/",prefix,"_LAB_predictions.tab"),sep="\t",quote=F)
     if (plot){
       w <- 16
